8ee9182fadf819abbadaf8b5d09e374aeeb79d9d
jirasinworasiri <aomjirasin457@gmail.com>
2024-10-15 11:21:49 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Welcome to My Vercel App</h1>
+      <p> This is a simple React app deployed on Vercel. </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vercel.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Vercel
         </a>
       </header>
     </div>
